8203df5dd1ba23772ab280ff52a1d226d57673f0
Panxin (Claire) Liu <157444624+Liupanx@users.noreply.github.com>
2025-10-31 16:46:04 -0700

    Time: 6154 ms (5.01%), Space: 178.4 MB (14.66%) - LeetHub

---
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -0,0 +1,45 @@
+import heapq
+def two_point_distance(point1,  point2) -> int:
+    # point1: [2, 1] point2: [3, 0]
+    distance = abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+    return distance
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        # Prim's Algorithm #heappop()
+        n = len(points)
+        visited = []*n
+
+        all_dis = {i:[] for i in range(n)} #distance, point
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = (two_point_distance(points[i], points[j]))
+                all_dis[i].append([dist, j])
+                all_dis[j].append([dist, i])
+        
+        result = 0
+        min_heap = [[0,0]] # cost, point
+
+        while len(visited) < n:
+            cost, point = heapq.heappop(min_heap)
+            if point in visited:
+                continue 
+            result += cost
+            visited.append(point)
+            for neighbor_cost, neighbor in all_dis[point]:
+                if neighbor not in visited:
+                    heapq.heappush(min_heap, [neighbor_cost, neighbor])
+        
+        return result
+
+
+
+
+        # all_dis = []
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         all_dis.append(two_point_distance(points[i], points[j]))
+        # all_dis.sort()
+
+        
+        
