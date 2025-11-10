53356cd1825c1f137e351c417f13f0e49a07993e
Panxin (Claire) Liu <157444624+Liupanx@users.noreply.github.com>
2025-11-10 00:09:39 -0800

    Time: 39 ms (72.96%), Space: 18.4 MB (36.14%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,28 +1,37 @@
-from collections import Counter
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        n, k = len(s), len(p)
-        if k > n: 
+        if len(p) > len(s):
             return []
 
-        pCount = Counter(p)
-        sCount = Counter(s[:k])
+        pCount, sCount = {}, {}
+        k = len(p)
+
+        # build p's frequency
+        for ch in p:
+            pCount[ch] = pCount.get(ch, 0) + 1
+
+        # build the first window from s
+        for ch in s[:k]:
+            sCount[ch] = sCount.get(ch, 0) + 1
 
         res = []
         if sCount == pCount:
             res.append(0)
 
-        for i in range(n - k):
-            left = s[i]
-            right = s[i + k]
+        l = 0
+        # slide the window
+        for r in range(k, len(s)):
+            # add right char
+            sCount[s[r]] = sCount.get(s[r], 0) + 1
 
+            # remove left char
+            left = s[l]
             sCount[left] -= 1
             if sCount[left] == 0:
-                del sCount[left]
-
-            sCount[right] = sCount.get(right, 0) + 1
+                sCount.pop(left)
+            l += 1
 
             if sCount == pCount:
-                res.append(i + 1)
+                res.append(l)
 
         return res
